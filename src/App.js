1edce3119d1b93496dbac962d5c27b3956037b5b
Pasindu Perakum <99235576+IT21160066@users.noreply.github.com>
2024-10-14 21:42:57 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import { Navigate, Route, Routes } from "react-router-dom";
+import Home from "../src/pages/Home";
+import User from "../src/pages/User";
+import AdminDashboard from "../src/pages/AdminDashboard";
+
+const USER_TYPES = {
+  PUBLIC_USER: "Public User",
+  REGISTERED_USER: "Registered User",
+  ADMIN_USER: "Admin User",
+};
+
+const CURRENT_USER_TYPE = USER_TYPES.REGISTERED_USER;
+
+function PublicElemet({ children }) {
+  return <>{children}</>;
+}
+
+function RegisteredElemet({ children }) {
+  if (
+    CURRENT_USER_TYPE === USER_TYPES.REGISTERED_USER ||
+    CURRENT_USER_TYPE === USER_TYPES.ADMIN_USER
+  ) {
+    return <>{children}</>;
+  } else {
+    return <Navigate to={"/"} />;
+  }
+}
+
+function AdmindElemet({ children }) {
+  if (CURRENT_USER_TYPE === USER_TYPES.ADMIN_USER) {
+    return <>{children}</>;
+  } else {
+    return (
+      <div>
+        <h3>You don't have access to this page...</h3>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <PublicElemet>
+              <Home />
+            </PublicElemet>
+          }
+        ></Route>
+        <Route
+          path="/user"
+          element={
+            <RegisteredElemet>
+              <User />
+            </RegisteredElemet>
+          }
+        ></Route>
+        <Route
+          path="/admin"
+          element={
+            <AdmindElemet>
+              <AdminDashboard />
+            </AdmindElemet>
+          }
+        ></Route>
+        <Route
+          path="*"
+          element={
+            <div>
+              <h1>Page not found!</h1>
+            </div>
+          }
+        ></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/AdminDashboard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AdmnDashboard = () => {
+  return <div>AdmnDashboard</div>;
+};
+
+export default AdmnDashboard;
